0243798cc928803c64dbaef7fd258e10229262e6
k-dang <dangkevin12@gmail.com>
2021-11-28 16:52:13 -0500

    add parser for interactive brokers

--- parser/main.go
+++ parser/main.go
@@ -62,3 +62,72 @@ func ParseQuestTrade(csvString string) (map[string][]Transaction, error) {
 
 	return transactionsByDate, nil
 }
+
+func ParseInteractive(csvString string) (map[string][]Transaction, error) {
+	rows := strings.Split(csvString, "\n")
+
+	transactionsByDate := make(map[string][]Transaction)
+
+	// parse first row to get correct column indexes
+	var td_i, act_i, sym_i, q_i, p_i, g_i, ac_i int
+	headerRow := rows[0]
+	for i, col := range strings.Split(headerRow, ",") {
+		switch col {
+		case "\"TradeDate\"":
+			td_i = i
+		case "\"Buy/Sell\"":
+			act_i = i
+		case "\"Symbol\"":
+			sym_i = i
+		case "\"Quantity\"":
+			q_i = i
+		case "\"TradePrice\"":
+			p_i = i
+		case "\"CostBasis\"":
+			g_i = i
+		case "\"AssetClass\"":
+			ac_i = i
+		}
+	}
+
+	for _, row := range rows[1:] {
+		if row == "" {
+			continue
+		}
+
+		cols := strings.Split(row, ",")
+		// go adds extra quotes for some reason
+		if cols[ac_i][1:len(cols[ac_i])-1] != "STK" {
+			continue
+		}
+
+		transactionDate := cols[td_i][1 : len(cols[td_i])-1]
+		action := cols[act_i][1 : len(cols[act_i])-1]
+		symbol := cols[sym_i][1 : len(cols[sym_i])-1]
+		quantity, err := strconv.ParseFloat(cols[q_i][1:len(cols[q_i])-1], 64)
+		price, err := strconv.ParseFloat(cols[p_i][1:len(cols[p_i])-1], 64)
+		gross, err := strconv.ParseFloat(cols[g_i][1:len(cols[g_i])-1], 64)
+
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		transaction := Transaction{
+			Symbol:          symbol,
+			Action:          action,
+			Gross:           gross,
+			Quantity:        quantity,
+			Price:           price,
+			TransactionDate: transactionDate,
+		}
+
+		if value, found := transactionsByDate[transactionDate]; found {
+			transactionsByDate[transactionDate] = append(value, transaction)
+		} else {
+			transactionsByDate[transactionDate] = []Transaction{transaction}
+		}
+	}
+
+	return transactionsByDate, nil
+}
