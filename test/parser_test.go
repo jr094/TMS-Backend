923a40d409b226b9fc75bd72c27d579ffeebd512
Jay Kumar <jaykumar094@outlook.com>
2022-01-26 21:17:15 -0800

    add testing framework

--- test/parser_test.go
+++ test/parser_test.go
@@ -6,28 +6,21 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestQuestradeParser(t *testing.T) {
 	curDir, _ := os.Getwd()
 	testFile := filepath.Join(curDir, "files/questrade.csv")
 	content, err := ioutil.ReadFile(testFile)
-	if err != nil || content == nil {
-		t.Errorf("Could not parse questrade file - ERR: %s", err)
-	}
-
+	assert.Nilf(t, err, "Could not parse questrade file - ERR: %s", err)
+	assert.NotNilf(t, content, "Could not parse questrade file - ERR: %s", err)
 	questradeParser := parser.QuestradeParser{}
 	symbols, transactionsByDate, err := questradeParser.ParseString(string(content))
-	if err != nil {
-		t.Errorf("Could not parse Questrade CSV string %s", err)
-	}
-
-	if _, ok := symbols["AAPL"]; !ok || len(symbols) != 1 {
-		t.Errorf("AAPL was not in stocklist after parse")
-	}
-
-	expectedCount := 1
-	if len(transactionsByDate) != 1 {
-		t.Errorf("Transaction count was %d but expected %d", len(transactionsByDate), expectedCount)
-	}
+	assert.Nilf(t, err, "Could not parse Questrade CSV string %s", err)
+	_, ok := symbols["AAPL"]
+	assert.Truef(t, ok, "AAPL was not in stocklist after parse")
+	assert.Equal(t, 1, len(symbols))
+	assert.Equal(t, 1, len(transactionsByDate), "Transaction count not 1")
 }
